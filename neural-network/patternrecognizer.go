5f87291c969e4449d493abcc612aed5b359745b5
Thane Vo <vothane@gmail.com>
2014-05-13 12:33:12 -0600

    fixes

--- neural-network/patternrecognizer.go
+++ neural-network/patternrecognizer.go
@@ -7,50 +7,51 @@ import (
 	//matrix "github.com/skelterjohn/go.matrix"
 )
 
-type PatternRecognizer struct{ weights []int }
+type PatternRecognizer struct{ weights []float64 }
 
 func New(n int) *PatternRecognizer {
-	ws := make([]int, n)
+	ws := make([]float64, n)
 	rand.Seed(time.Now().Unix())
 	for i := range ws {
-		if r := rand.Intn(2); r == 0 {
-			ws[i] = -1
+		if r := rand.Intn(1); r == 0 {
+			ws[i] = float64(-1)
 		} else {
-			ws[i] = 1
+			ws[i] = float64(1)
 		}
 	}
 	patternrecog := PatternRecognizer{weights: ws}
 	return &patternrecog
 }
 
-func (patternrecog *PatternRecognizer) Feedforward(inputs []int) int {
-	sum := 0
+func (patternrecog *PatternRecognizer) Feedforward(inputs []float64) float64 {
+	sum := 0.0
 	for i := range patternrecog.weights {
 		sum += inputs[i] * patternrecog.weights[i]
 	}
-	return activate(sum)
+	return patternrecog.activate(sum)
 }
 
-func (patternrecog *PatternRecognizer) activate(sum int) int {
+func (patternrecog *PatternRecognizer) activate(sum float64) float64 {
+	var val float64
 	if sum > 0 {
-		return 1
+		val = 1.0
 	} else {
-		return -1
+		val = -1.0
 	}
-	return 1
+	return val
 }
 
-func (patternrecog *PatternRecognizer) Train(inputs []float64, desired int) {
-	guess := Feedforward(inputs)
+func (patternrecog *PatternRecognizer) Train(inputs []float64, desired float64) {
+	guess := patternrecog.Feedforward(inputs)
 	error := desired - guess
 	for i := range patternrecog.weights {
-		patternrecogweights[i] += c * error * inputs[i]
+		patternrecog.weights[i] += 0.01 * error * inputs[i]
 	}
 }
 
 func main() {
 	patternrecog := New(3)
-	point := []int{50, -12, 1}
+	point := []float64{50, -12, 1}
 	result := patternrecog.Feedforward(point)
 	fmt.Println(result)
 }
