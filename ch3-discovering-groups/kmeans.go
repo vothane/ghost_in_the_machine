e56a443fed33749251c03b6d91c00a0db7d0b367
Thane Vo <vothane@gmail.com>
2014-06-03 23:42:00 -0600

    add fucntion to find the minimun and maximum of both x & y in data points

--- ch3-discovering-groups/kmeans.go
+++ ch3-discovering-groups/kmeans.go
@@ -3,9 +3,10 @@ package main
 import (
 	"fmt"
 	"math"
+	"sort"
 )
 
-func sumcalc(coll []float64, summer func(x float64) float64) float64 {
+func sumCalc(coll []float64, summer func(x float64) float64) float64 {
 	var sum float64 = 0.0
 
 	for _, x := range coll {
@@ -14,7 +15,7 @@ func sumcalc(coll []float64, summer func(x float64) float64) float64 {
 	return sum
 }
 
-func sumofprods(x, y []float64) float64 {
+func sumOfProds(x, y []float64) float64 {
 	var sum float64 = 0.0
 
 	for i := range x {
@@ -24,26 +25,48 @@ func sumofprods(x, y []float64) float64 {
 }
 
 func pearson(v1, v2 []float64) float64 {
-	sum1 := sumcalc(v1, func(x float64) float64 { return x })
-	sum2 := sumcalc(v2, func(x float64) float64 { return x })
+	sum1 := sumCalc(v1, func(x float64) float64 { return x })
+	sum2 := sumCalc(v2, func(x float64) float64 { return x })
 
-	sum1sq := sumcalc(v1, func(x float64) float64 { return x * x })
-	sum2sq := sumcalc(v2, func(x float64) float64 { return x * x })
+	sum1sq := sumCalc(v1, func(x float64) float64 { return x * x })
+	sum2sq := sumCalc(v2, func(x float64) float64 { return x * x })
 
-	psum := sumofprods(v1, v2)
+	psum := sumOfProds(v1, v2)
 
 	num := psum - (sum1 * sum2 / float64(len(v1)))
-	den := math.Sqrt((sum1sq - math.Pow(sum1, 2) / float64(len(v1))) * (sum2sq-math.Pow(sum2, 2) / float64(len(v1))))
+	den := math.Sqrt((sum1sq - math.Pow(sum1, 2)/float64(len(v1))) * (sum2sq - math.Pow(sum2, 2)/float64(len(v1))))
 
 	if den == 0 {
 		return 0.0
 	}
+	return 1.0 - num/den
+}
 
-	return 1.0 - num / den
+func colMinMax(data [][]float64) [2][2]float64 {
+	var colData [2][7]float64
+	var minmax [2][2]float64
+
+	for i, point := range data {
+		for j, item := range point {
+			colData[j][i] = item
+		}
+	}
+
+	for i := range colData {
+		sort.Float64s((colData[i])[:])
+	}
+
+	for i, datum := range colData {
+		minmax[i] = [...]float64{datum[0], datum[6]}
+	}
+	return minmax
 }
 
 func main() {
 	v1 := []float64{0.0, 1.0, 0.0, 0.0, 3.0, 3.0, 0.0, 0.0, 3.0, 0.0, 6.0, 0.0, 1.0, 0.0, 4.0, 3.0, 0.0, 0.0, 0.0, 0.0, 0.0, 4.0, 0.0}
 	v2 := []float64{0.0, 2.0, 1.0, 0.0, 6.0, 2.0, 1.0, 0.0, 4.0, 5.0, 25.0, 0.0, 0.0, 0.0, 6.0, 12.0, 4.0, 2.0, 1.0, 4.0, 0.0, 3.0, 0.0}
+	rows := [][]float64{{1.0, 1.0}, {1.5, 2.0}, {3.0, 4.0}, {5.0, 7.0}, {3.5, 5.0}, {4.5, 5.0}, {3.5, 4.5}}
 	fmt.Println(pearson(v1, v2))
+	fmt.Println(rows)
+	fmt.Println(colMinMax(rows))
 }
